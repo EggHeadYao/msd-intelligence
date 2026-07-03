bbfe4a6361e79c75ccbf7e54d6ad989f3619e7ee
egghead <egghead_yao@sjtu.edu.cn>
2026-07-03 19:42:53 +0000

    feat: mapreduce/final map

---
+++ artistdistance/src/main/java/artistdistance/mapreduce/BfsFinalMapper.java
@@ -0,0 +1,18 @@
+package artistdistance.mapreduce;
+
+import artistdistance.bfs.BfsRules;
+import artistdistance.schema.ArtistDistance;
+import artistdistance.schema.BfsVertex;
+import java.io.IOException;
+import org.apache.avro.mapred.AvroKey;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public final class BfsFinalMapper
+    extends Mapper<AvroKey<BfsVertex>, NullWritable, AvroKey<ArtistDistance>, NullWritable> {
+  @Override
+  protected void map(AvroKey<BfsVertex> key, NullWritable value, Context context)
+      throws IOException, InterruptedException {
+    context.write(new AvroKey<>(BfsRules.toArtistDistance(key.datum())), NullWritable.get());
+  }
+}
