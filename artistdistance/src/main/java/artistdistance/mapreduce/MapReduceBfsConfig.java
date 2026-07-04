c70a2fb30991f479a5a40a8e66684b2e57920f23
egghead <egghead_yao@sjtu.edu.cn>
2026-07-04 15:12:25 +0000

    feat: mapred config

---
+++ artistdistance/src/main/java/artistdistance/mapreduce/MapReduceBfsConfig.java
@@ -0,0 +1,9 @@
+package artistdistance.mapreduce;
+
+public final class MapReduceBfsConfig {
+  public static final String SOURCE_ID = "artistdistance.bfs.source";
+  public static final String MAX_ITERATIONS = "artistdistance.bfs.max.iterations";
+  public static final String REDUCERS = "artistdistance.bfs.reducers";
+
+  private MapReduceBfsConfig() {}
+}
